4f1aa16360057d9df562d95d9070721d95d08b7c
Sifflion <dami.m.gonza@gmail.com>
2022-06-07 19:39:01 -0300

    test

--- src/app.js
+++ src/app.js
@@ -14,17 +14,17 @@ app.set('port',process.env.PORT);
 app.set("view engine", "ejs");
 
 //Middlewares used
-app.use(cors({ origin: "https://regalitosnaomanager.herokuapp.com", credentials: true }))
+app.use(cors())
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use(express.static('public')); 
-app.use('/images',cors(), express.static('images'));
+app.use('/images', express.static('images'));
 
-app.use('/api/products',cors(),ProductsRoutes); //Goes to products
-app.use('/api/users',cors(),UserRoutes); //Goes to users
-app.use('/api/messages',cors(),MessageRoutes); //Goes to messages
+app.use('/api/products',ProductsRoutes); //Goes to products
+app.use('/api/users',UserRoutes); //Goes to users
+app.use('/api/messages',MessageRoutes); //Goes to messages
 
 export default app;
