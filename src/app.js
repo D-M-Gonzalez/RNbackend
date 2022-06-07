05aca784c11ca1d68f1b6a0e88bcde12f0bacbbe
Sifflion <dami.m.gonza@gmail.com>
2022-06-07 19:16:55 -0300

    test

--- src/app.js
+++ src/app.js
@@ -15,16 +15,17 @@ app.set("view engine", "ejs");
 
 //Middlewares used
 app.use(cors());
+app.options('*', cors())
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use(express.static('public')); 
-app.use('/images', express.static('images'));
+app.use('/images',cors(), express.static('images'));
 
-app.use('/api/products',ProductsRoutes); //Goes to products
-app.use('/api/users',UserRoutes); //Goes to users
-app.use('/api/messages',MessageRoutes); //Goes to messages
+app.use('/api/products',cors(),ProductsRoutes); //Goes to products
+app.use('/api/users',cors(),UserRoutes); //Goes to users
+app.use('/api/messages',cors(),MessageRoutes); //Goes to messages
 
 export default app;
